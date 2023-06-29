107ab9f701ad9ab7e2f578098f66b945f81a2952
iSoldato <128804052+iSoldato@users.noreply.github.com>
2023-06-29 19:38:06 +0400

    Create arrayUtils.js

---
+++ arrayUtils.js
@@ -0,0 +1,14 @@
+// arrayUtils.js
+
+function findMax(array) {
+  return Math.max(...array);
+}
+
+function findMin(array) {
+  return Math.min(...array);
+}
+
+module.exports = {
+  findMax,
+  findMin,
+};
